1e57eed8f53fa09a370e3ccecae02958008fb18b
root <root@THEGLIZZ>
2025-06-26 10:20:31 -0400

    Created short load_train nb

--- pointnet2_keypoint_regressor.py
+++ pointnet2_keypoint_regressor.py
@@ -15,11 +15,13 @@ class get_model(nn.Module):
         self.sa3 = PointNetSetAbstraction(npoint=None, radius=None, nsample=None, in_channel=256 + 3, mlp=[256, 512, 1024], group_all=True)
 
         self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.drop1 = nn.Dropout(0.2)
+        #self.bn1 = nn.BatchNorm1d(512) #remove batchnorm for testing
+        #self.drop1 = nn.Dropout(0.2)
+        self.drop1 = nn.Identity() #replace dropout with identity for testing
         self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.drop2 = nn.Dropout(0.3)
+        #self.bn2 = nn.BatchNorm1d(256)
+        #self.drop2 = nn.Dropout(0.3)
+        self.drop2 = nn.Identity()
         self.fc3 = nn.Linear(256, self.num_keypoints * 3)
 
     def forward(self, xyz):
@@ -35,8 +37,10 @@ class get_model(nn.Module):
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
 
         x = l3_points.view(B, 1024)
-        x = self.drop1(F.relu(self.bn1(self.fc1(x))))
-        x = self.drop2(F.relu(self.bn2(self.fc2(x))))
+        #x = self.drop1(F.relu(self.bn1(self.fc1(x))))
+        #x = self.drop2(F.relu(self.bn2(self.fc2(x))))
+        x = F.relu(self.fc1(x)) #same as below
+        x = F.relu(self.fc2(x)) #changed for no-droupout
         x = self.fc3(x)
         x = x.view(B, self.num_keypoints, 3)  # reshape to keypoint coordinates
 
@@ -53,52 +57,3 @@ class get_loss(nn.Module):
 
 
 
-MSG = '''
-class get_model(nn.Module):
-    def __init__(self, num_keypoints, normal_channel=True):
-        super(get_model, self).__init__()
-        in_channel = 3 if normal_channel else 0
-        self.num_keypoints = num_keypoints
-        self.normal_channel = normal_channel
-
-        # Keep all SA layers unchanged          
-        self.sa1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [16, 32, 128], in_channel,
-                                              [[32, 32, 64], [64, 64, 128], [64, 96, 128]])
-        self.sa2 = PointNetSetAbstractionMsg(128, [0.2, 0.4, 0.8], [32, 64, 128], 320,
-                                              [[64, 64, 128], [128, 128, 256], [128, 128, 256]])
-        self.sa3 = PointNetSetAbstraction(None, None, None, 640 + 3, [256, 512, 1024], True)
-
-        # Switching from MSG to SSG to shrink model + improve efficiency. (MSG seems to run on CPU for some reason)
-
-
-        #MLP Half:
-        self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        #self.drop1 = nn.Dropout(0.4) #Potentially lower this to improve train loss >> 0.2
-        self.drop1 = nn.Dropout(0.2)
-        self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        #self.drop2 = nn.Dropout(0.5) #Same here >> 0.3
-        self.drop2 = nn.Dropout(0.3)
-        self.fc3 = nn.Linear(256, num_keypoints * 3)
-
-    def forward(self, xyz):
-        B, _, _ = xyz.shape
-        if self.normal_channel:
-            norm = xyz[:, 3:, :]
-            xyz = xyz[:, :3, :]
-        else:
-            norm = None
-
-        l1_xyz, l1_points = self.sa1(xyz, norm)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-
-        x = l3_points.view(B, 1024)
-        x = self.drop1(F.relu(self.bn1(self.fc1(x))))
-        x = self.drop2(F.relu(self.bn2(self.fc2(x))))
-        x = self.fc3(x)
-        x = x.view(B, self.num_keypoints, 3)  # Reshape to keypoint predictions
-
-        return x
-'''
